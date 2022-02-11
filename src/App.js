d7b04e82c147f9b4d1349fdde8cbfbf4d2359ae6
minshadk <minshadk13@gmail.com>
2022-02-11 22:31:34 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import PersonIcon from '@mui/icons-material/Person';
+
+import Home from "./pages/home/Home";
+
 function App() {
-  return (
-    <div >
-     Hellow
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Topbar from "../../components/Topbar";
+const Home = () => {
+  return (
+    <dvi>
+      <Topbar />
+    </dvi>
+  );
+};
+
+export default Home;
